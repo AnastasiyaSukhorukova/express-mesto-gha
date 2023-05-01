2834ee0b9ba377b7fb2bb44fae70062ea36c9b4b
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-02 00:00:18 +0300

    связала БД и JS

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const app = express();
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // по вебинару
