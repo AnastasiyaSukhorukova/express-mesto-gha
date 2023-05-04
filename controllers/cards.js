a0c05d9b4acc2cfb9c42012f11f359fd944dfcd9
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-05 00:06:53 +0300

    Исправление ошибок автотестов

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,8 +10,7 @@ const {
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id });
-  console.log(req.user._id)
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
-const { ERROR_CODE } = require('../constants/constants');
 
 const userRouter = require('./users');
 const cardsRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
-router.use('/*', (req, res) => {
-  res.status(ERROR_CODE).send({ message: 'Ошибка. Введите корректный URL' });
-});
 
 module.exports = router;
