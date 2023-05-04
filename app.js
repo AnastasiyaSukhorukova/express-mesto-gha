81c658ae9744053bc93e1def1fc29486813c7e3b
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-04 23:43:59 +0300

    Исправление ошибок автотестов

--- app.js
+++ app.js
@@ -4,17 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту: ${PORT}`);
-});
-
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-
-app.use('/', require('./routes/index'));
 
 // id созданного пользователя
 app.use((req, res, next) => {
@@ -22,4 +16,10 @@ app.use((req, res, next) => {
     _id: '64517881e1a3e41c85fba33b',
   };
   next();
+});
+
+app.use('/', require('./routes/index'));
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту: ${PORT}`);
 });
