ae5676569ab1d3432a374d4986027340ab063618
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-05 19:18:55 +0300

    корректировка файла app.js

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const router = require('./routes/index');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(express.json());
-
-// id созданного пользователя
-app.use((req, res, next) => {
+const idMiddleware = (req, res, next) => {
   req.user = {
     _id: '64517881e1a3e41c85fba33b',
   };
-  next();
-});
 
-app.use(router);
+  next();
+};
+
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
+
+app.use(idMiddleware);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
