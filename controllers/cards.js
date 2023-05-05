04f16d753b2c304f779b7900f82c710e10fd50ca
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-05 19:47:51 +0300

    исправила синтаксические ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,14 +9,14 @@ const {
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => {res.status(200).send(cards)})
-    .catch(() => res.status(ERROR_CODE_DEFAULT).send({ message: dafaultErrorMessage }));
+    .then((cards) => { res.status(200).send(cards) })
+    .catch(() => { res.status(ERROR_CODE_DEFAULT).send({ message: dafaultErrorMessage })});
 };
 
 module.exports.removeCardId = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .orFail()
-    .then((card) => {res.status(200).send(card)})
+    .then((card) => { res.status(200).send(card) })
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
         return res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Карточка не найдена.' });
@@ -33,8 +33,8 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => {res.status(201).send(card)
-    console.log(card)})
+    .then((card) => { res.status(201).send(card)
+    console.log(card) })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные в методы создания карточки.' });
@@ -46,7 +46,7 @@ module.exports.createCard = (req, res) => {
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .orFail()
-    .then((card) => {res.status(200).send(card)})
+    .then((card) => { res.status(200).send(card) })
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
         return res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Карточка не найдена.' });
@@ -61,7 +61,7 @@ module.exports.likeCard = (req, res) => {
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .orFail()
-    .then((card) => {res.status(200).send(card)})
+    .then((card) => { res.status(200).send(card) })
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
         return res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Карточка не найдена.' });
