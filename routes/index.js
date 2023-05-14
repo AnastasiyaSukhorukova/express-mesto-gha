dea40e41a87bef354676afd27642c365036855de
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-14 18:32:42 +0300

    добавила joi

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
+const { errors } = require('celebrate');
 const { ERROR_CODE_NOT_FOUND } = require('../constants/constants');
 
 const userRouter = require('./users');
 const cardsRouter = require('./cards');
 const { login, createUser } = require('../controllers/users');
+const { createUserJoi, loginJoi } = require('../middlewares/celebrate');
 
 const authMiddleware = require('../middlewares/auth');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signin', loginJoi, login);
+router.post('/signup', createUserJoi, createUser);
 
 router.use(authMiddleware);
 router.use('/users', userRouter);
@@ -16,5 +18,6 @@ router.use('/cards', cardsRouter);
 router.use('/*', (req, res) => {
   res.status(ERROR_CODE_NOT_FOUND).send({ message: '404: Not Found' });
 });
+router.use(errors({ message: 'Ошибка валидации данных!' }));
 
 module.exports = router;
