bd16c0db46c8e3e62e7dcfb9d4d0a8a2b84df6e9
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-16 19:35:17 +0300

    исправила app.use

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes/index');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -9,7 +8,7 @@ app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
-app.use(router);
+app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
