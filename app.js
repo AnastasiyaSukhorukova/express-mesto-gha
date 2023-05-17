523d056426d156ff73d56b61cccacfa9a4d5a7d0
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-17 20:27:37 +0300

    добавила обработку ошибок в app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const catchErrorsMiddleware = require('./middlewares/errors');
+
 const app = express();
+
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
@@ -9,6 +12,8 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.use('/', require('./routes/index'));
+
+app.use(catchErrorsMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
